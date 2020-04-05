ab5c64bbfcdf1846cece674f8b60f568cc784000
Eduard Besjentsev <fc.puntje@gmail.com>
2020-04-05 02:57:59 +0200

    Added basic modular file structure to back-end and possibility to run it with DEBUG flags.

---
+++ server/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+
+const indexRouter = require("./routes/index");
+const apiRouter = require("./routes/api");
+
+const app = express();
+
+app.use(express.json());
+app.use("/", indexRouter);
+app.use("/api", apiRouter);
+
+module.exports = app;

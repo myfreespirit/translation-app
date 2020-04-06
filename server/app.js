17be4bc1e288b939e002cbe17cc392c1b758db3a
Eduard Besjentsev <fc.puntje@gmail.com>
2020-04-06 07:00:51 +0200

    Serve static files from the dist directory after 'npm run build' in production.

--- server/app.js
+++ server/app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/dist')));
+}
+
 // TODO: description of API with swagger
 app.use("/", indexRouter);
 app.use("/api", apiRouter);

c551eb69f765bb9903beddf7237bcc78785079bb
Eduard Besjentsev <fc.puntje@gmail.com>
2020-04-06 07:43:48 +0200

    Add static configuration in static.json

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const history = require('connect-history-api-fallback');
+const history = require("connect-history-api-fallback");
+const path = require("path");
 
 // const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
@@ -10,9 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(history());
-    app.use(express.static(path.join(__dirname, '/dist')));
+if (process.env.NODE_ENV === "production") {
+  app.use(history());
+  app.use(express.static(path.join(__dirname, "/dist")));
 }
 
 // TODO: description of API with swagger
